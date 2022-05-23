cd0839ed62baedd70ee929daf5254e28f169051a
luciano-im <luchisds@gmail.com>
2022-05-23 12:27:20 -0300

    💬 Allow existing text translations

--- custom_url/models.py
+++ custom_url/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
+from django.utils.translation import gettext_lazy as _
 
 class CustomUrl(models.Model):
     CONTENT_TYPES = [
@@ -15,7 +16,7 @@ class CustomUrl(models.Model):
         ('image/svg+xml', 'SVG'),
         ('application/pdf', 'PDF'),
     ]
-    url = models.CharField(max_length=100, db_index=True, unique=True, verbose_name='URL')
+    url = models.CharField(max_length=100, db_index=True, unique=True, verbose_name=_('URL'))
     file = models.FileField(
         upload_to='custom_url',
         validators=[
@@ -23,9 +24,10 @@ class CustomUrl(models.Model):
                 allowed_extensions=['txt', 'csv', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'gif', 'jpg', 'jpeg', 'png', 'tif', 'tiff', 'svg', 'pdf']
             )
         ],
-        verbose_name='File'
+        verbose_name=_('File'),
+        help_text = _('Allowed format files: Plain Text, CSV, Excel, Word, PowerPoint, GIF, JPEG, PNG, TIFF, SVG, PDF')
     )
-    file_type = models.CharField(max_length=50, choices=CONTENT_TYPES, verbose_name='File Type')
+    file_type = models.CharField(max_length=50, choices=CONTENT_TYPES, verbose_name=_('File Type'))
 
     def __str__(self):
         return self.url
